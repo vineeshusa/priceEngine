9bdeff1ce5fab59d1842328138c0826e1734404a
vineeshusa <vineeshusa@gmail.com>
2018-01-09 14:31:50 -0500

    Add files via upload

--- pricingEngine/src/com/org/pricingEngine/filter/HighPriceFilter.java
+++ pricingEngine/src/com/org/pricingEngine/filter/HighPriceFilter.java
@@ -1,6 +1,10 @@
 package com.org.pricingEngine.filter;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.OptionalDouble;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 import com.org.pricingEngine.model.SurveyPrice;
 
@@ -9,8 +13,24 @@ public class HighPriceFilter implements Filter {
 	@Override
 	public List<SurveyPrice> filter(List<SurveyPrice> surveyPrices) {
 		// TODO Auto-generated method stub
-		
-		return surveyPrices;
+		List <Double> removablePrices = new ArrayList<> ();
+		OptionalDouble avg = surveyPrices.stream().mapToDouble(p ->p.getPrice()).average();
+		List<Double> prices = surveyPrices.stream().map (p -> p.getPrice()).collect(Collectors.toList());
+		List<SurveyPrice> filteredList = null;
+		for (Double price : prices) {
+			if (price > avg.getAsDouble()*3/2) {
+				removablePrices.add(price);
+			}
+		}
+		for (Double price: removablePrices) {
+			
+			filteredList = surveyPrices.stream().filter(removeIfPriceEqual(price)).collect(Collectors.toList());
+			
+		}
+		return filteredList;
 	}
-
+	
+	private Predicate<SurveyPrice> removeIfPriceEqual(Double price) {
+		return p->p.getPrice() != price;
+	}
 }
--- pricingEngine/src/com/org/pricingEngine/filter/LowPriceFilter.java
+++ pricingEngine/src/com/org/pricingEngine/filter/LowPriceFilter.java
@@ -1,6 +1,10 @@
 package com.org.pricingEngine.filter;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.OptionalDouble;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 import com.org.pricingEngine.model.SurveyPrice;
 
@@ -9,7 +13,27 @@ public class LowPriceFilter implements Filter {
 	@Override
 	public List<SurveyPrice> filter(List<SurveyPrice> surveyPrices) {
 		// TODO Auto-generated method stub
-		return null;
+		//surveyPrices.stream().map(p ->p.getPrice()).collect(Collectors.toList()).stream().mapToDouble(a->a).average();
+		List <Double> removablePrices = new ArrayList<> ();
+		OptionalDouble avg = surveyPrices.stream().mapToDouble(p ->p.getPrice()).average();
+		List<Double> prices = surveyPrices.stream().map (p -> p.getPrice()).collect(Collectors.toList());
+		List<SurveyPrice> filteredList = null;
+		for (Double price : prices) {
+			if (price < avg.getAsDouble()/2) {
+				removablePrices.add(price);
+			}
+		}
+		
+		for (Double price: removablePrices) {
+			
+			filteredList = surveyPrices.stream().filter(removeIfPriceEqual(price)).collect(Collectors.toList());
+			
+		}
+		return filteredList;
+	}
+	
+	private Predicate<SurveyPrice> removeIfPriceEqual(Double price) {
+		return p->p.getPrice() != price;
 	}
 
 }
